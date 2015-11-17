7de236b9f2ebb7b9c9b44df2578c3e8af8b296f2
bennygr <bennygr@gmx.net>
2015-11-17 11:13:04 +0100

    Fixed disqus title binding 404 article handling

--- app/controller/articleController.js
+++ app/controller/articleController.js
@@ -1,11 +1,13 @@
 
 bnyBlogApp.controller('articleController', function($scope,$sce,$routeParams,$location,Article){
+	$scope.articleLoaded = false;
 	$scope.url = "http://" + $location.host() + $location.path();
 	$scope.loadArticle = function(){
 		var name = $routeParams.param;
 		//Getting the article 
 		var article = Article.get({articleName: name},function(articles,responseHeader){       
 			$scope.article = article;
+			$scope.articleLoaded = true;
 		});    
 	};
 
--- app/controller/mainController.js
+++ app/controller/mainController.js
@@ -1,4 +1,3 @@
 
 bnyBlogApp.controller('MainController', function($scope,$location){
-	$scope.path = $location.path();
 });
