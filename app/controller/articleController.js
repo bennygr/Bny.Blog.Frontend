11cfdef72eb5f27b06068e66d19085ecf5137659
bennygr <bennygr@gmx.net>
2015-11-17 20:41:33 +0100

    disqus comment count

--- app/controller/articleController.js
+++ app/controller/articleController.js
@@ -2,12 +2,17 @@
 bnyBlogApp.controller('articleController', function($scope,$sce,$routeParams,$location,Article){
 	$scope.articleLoaded = false;
 	$scope.url = "http://" + $location.host() + $location.path();
+	$scope.$on('$routeChangeSuccess', function(scope, next, current){
+		window.DISQUSWIDGETS = undefined;
+		$.getScript("//bennygr.disqus.com/count.js");
+	   });
 	$scope.loadArticle = function(){
 		var name = $routeParams.param;
 		//Getting the article 
 		var article = Article.get({articleName: name},function(articles,responseHeader){       
 			$scope.article = article;
 			$scope.articleLoaded = true;
+			$.getScript("//bennygr.disqus.com/count.js");
 		});    
 	};
 
