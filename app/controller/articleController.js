fb36c541312b8ba5a65d34047bf0d585b1d11bf3
Benny <bennygr@gmx.net>
2015-11-22 22:14:03 +0100

    publishing preview articles

--- app/controller/articleController.js
+++ app/controller/articleController.js
@@ -5,7 +5,7 @@ bnyBlogApp.controller('articleController', function($scope,$sce,$routeParams,$lo
 	$scope.$on('$routeChangeSuccess', function(scope, next, current){
 		window.DISQUSWIDGETS = undefined;
 		$.getScript("//bennygr.disqus.com/count.js");
-	   });
+	   })
 	$scope.loadArticle = function(){
 		var name = $routeParams.param;
 		//Getting the article 
@@ -14,7 +14,7 @@ bnyBlogApp.controller('articleController', function($scope,$sce,$routeParams,$lo
 			$scope.articleLoaded = true;
 			$.getScript("//bennygr.disqus.com/count.js");
 		});    
-	};
+	}
 
 	$scope.toTrustedHtml = function(html){
 		return $sce.trustAsHtml(html);
--- app/controller/articlePreviewController.js
+++ app/controller/articlePreviewController.js
@@ -1,8 +1,16 @@
 
-bnyBlogApp.controller('articlePreviewController', function($scope,$sce,$routeParams,$location,ArticlePreview){
+bnyBlogApp.controller('articlePreviewController', function($scope,
+														   $sce,
+														   $routeParams,
+														   $location,
+														   $http,
+														   ArticlePreview){
+
 	$scope.articleLoaded = false;
 	$scope.url = "http://" + $location.host() + $location.path();
 	$scope.isPreview = true;
+	$scope.publishError = null;
+
 	$scope.loadArticle = function(){
 		var code = $routeParams.previewCode;
 		//Getting the article 
@@ -11,6 +19,24 @@ bnyBlogApp.controller('articlePreviewController', function($scope,$sce,$routePar
 			$scope.articleLoaded = true;
 		});    
 	};
+
+	$scope.publishArticle = function(){
+		if(confirm("Do you really want to publish this article?")){
+			$scope.publishError = null;
+			$http({
+				method: 'GET',
+				url: '/bnyapi/articles/preview/publish/' + $scope.article.metaData.previewCode
+			}).then(function success(response){
+				$location.path("/article/" + $scope.article.fileName);
+			}, function error(response){
+				$scope.publishError = response.status;
+			});
+		}
+	}
+
+	$scope.clearError = function(){
+		$scope.publishError = null;	
+	}
 
 	$scope.toTrustedHtml = function(html){
 		return $sce.trustAsHtml(html);
